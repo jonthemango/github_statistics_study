051719e038c15a613cc809fd2e179ded027ebb62
Isaac Doré <swessmusic@gmail.com>
2018-12-06 16:28:15 -0500

    Update program output

--- stats.py
+++ stats.py
@@ -5,20 +5,30 @@ EXPLICIT_DATA_SET = loadJsonFile('reports/explicit_data.json')
 IMPLICIT_DATA_SET = loadJsonFile('reports/implicit_data.json')
 SAMPLE_DATA_SET = loadJsonFile('reports/sample_data.json')
 
-
 N = len(SAMPLE_DATA_SET)
-P =  len(EXPLICIT_DATA_SET) / N
+P = len(EXPLICIT_DATA_SET) / N
 
-MEAN = N*P
-VARIANCE = MEAN * (1 - P)
-STANDARD_DEVIATION = math.sqrt(VARIANCE)
-MEDIAN = math.floor(MEAN)
+
+## Calculating proper sample variance
+runningSum = 0
+for data in SAMPLE_DATA_SET:
+    x = 0
+    if data in EXPLICIT_DATA_SET:
+        x = 1
+    runningSum += (x-P)**2
+
+sampleVariance = runningSum/N
+
+
+MEAN = P
+STANDARD_DEVIATION = math.sqrt(sampleVariance)
+MEDIAN = 1
 
 data = {
-    "Mean" : MEAN,
-    "Variance" : VARIANCE,
-    "Standard Deviation" : STANDARD_DEVIATION,
-    "Median" : MEDIAN,
+    "Sample Mean" : MEAN,
+    "Sample Variance" : sampleVariance,
+    "Sample Standard Deviation" : STANDARD_DEVIATION,
+    "Sample Median" : MEDIAN,
     "N": N,
     "P": P
 }
